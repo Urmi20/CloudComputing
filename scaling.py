8ce3e3b335a0b7c7603b04d9db3386864bfa6645
felipe-clark <felipe.clark@gmail.com>
2018-11-15 23:54:55 -0500

    Final adjustments before integration test

--- scaling.py
+++ scaling.py
@@ -1,8 +1,9 @@
+import time
 from app.tools.scalingTools import ScalingTool
 from app.tools.dbTools import DataBaseManager
 
 while True:
-    #time.sleep(10)
+    time.sleep(10)
     # Get database scaling settings
     dbm = DataBaseManager(False)
     up_scale_factor, down_scale_factor, instance_start_load, instance_termination_load, mode = dbm.get_scaling_settings()
@@ -10,7 +11,7 @@ while True:
     if mode != 'automatic':
         continue
 
-    # Get number of instances on load balancer
+    # Get number of instances in service on load balancer
     total_running_instances = ScalingTool.get_number_of_in_service_instances_in_load_balancer()
 
     # Read load balancer average instance loads
