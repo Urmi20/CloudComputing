02f58293fafdfa00de33543ad3565bd8b458f71d
Ritam Haldar <haldarritam95@gmail.com>
2018-11-16 18:17:21 -0500

    Updated the new AMI-ID

--- app/tools/scalingTools.py
+++ app/tools/scalingTools.py
@@ -85,7 +85,7 @@ class ScalingTool:
                     },
                 },
             ],
-            ImageId='ami-05ce302e08cc9baa3',
+            ImageId='ami-0189620e379752a8e',
             InstanceType='t3.small',
             MaxCount=1,
             MinCount=1,
