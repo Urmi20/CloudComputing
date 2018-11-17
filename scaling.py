bf90db6a6a83a49531f441750c498fcec9d06cec
Ritam Haldar <haldarritam95@gmail.com>
2018-11-16 20:50:50 -0500

    Reverted the log changes

--- scaling.py
+++ scaling.py
@@ -1,19 +1,9 @@
 import time
 from app.tools.scalingTools import ScalingTool
 from app.tools.dbTools import DataBaseManager
-import logging
-from systemd.journal import JournaldLogHandler
 
 
-logger = logging.getLogger(__name__)
-
-journald_handler = JournaldLogHandler()
-journald_handler.setFormatter(logging.Formatter(
-    '[%(levelname)s] %(message)s'))
-logger.addHandler(journald_handler)
-logger.setLevel(logging.INFO)
-
-logger.info('Scaling application has started...')
+print('Scaling application has started...')
 
 while True:
     time.sleep(10)
